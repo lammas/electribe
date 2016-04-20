c1df93d65239e360ea7318920a83f88293b3a1f7
lammas <lammas@gmail.com>
2016-04-20 13:27:20 +0300

    Add: module.exports

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ var Sample = require('./src/sample');
 var Slice = require('./src/slice');
 var SampleData = require('./src/sampledata');
 
-console.log('GlobalParameters length: ', GlobalParameters.length(), 192);
-console.log('Pattern length: ', Pattern.length(), 4280);
-
 var ESXFile = new Format()
 	.buffer('header', 32)
 	.nest('global', GlobalParameters)
@@ -40,53 +37,55 @@ var ESXFile = new Format()
 	.buffer('_unknown5', 114176) // this is all 0x00
 	.buffer('sampledata', 'eof', SampleData);
 
-var TESTFILE = 'data/ESX-Factory-Data.esx';
-// var TESTFILE = 'data/saved.esx';
-fs.readFile(TESTFILE, function(err, buffer) {
-	if (err)
-		throw err;
+module.exports = ESXFile;
 
-	console.time('parse');
-	var result = ESXFile.parse(buffer);
-	if ('songevents' in result)
-		result.songevents.parse(result.songs);
-	if ('sampledata' in result)
-		result.sampledata.parse(result);
-	console.timeEnd('parse');
-
-	function findSampleByName(name, samples) {
-		for (var i=0; i<samples.length; i++) {
-			if (!samples[i])
-				continue;
-			if (samples[i].sample.name.value == name)
-				return samples[i];
-		}
-		return null;
-	}
-
-	function saveBuffer(name, buffer) {
-		var fd = fs.openSync(name, 'w');
-		fs.writeSync(fd, buffer, 0, buffer.length);
-		fs.closeSync(fd);
-	}
-
-	console.time('inspect');
-	// console.log(result);
-	// console.log(require('util').inspect(result, { depth: null }));
-	// console.log(require('util').inspect(result.songs, { depth: null }));
-	// console.log(require('util').inspect(result.songevents, { depth: null }));
-	// console.log(require('util').inspect(result.monoSampleHeaders, { depth: null }));
-	// console.log(require('util').inspect(result.stereoSampleHeaders, { depth: null }));
-	// console.log(require('util').inspect(result.slices, { depth: null }));
-	// console.log(require('util').inspect(result.sampledata, { depth: null }));
-	console.timeEnd('inspect');
-
-	var sample = findSampleByName('PercLP-1', result.sampledata.samples);
-	if (sample) {
-		console.log('Investigating sample: ', sample);
-		// saveBuffer('./perclp1.wav', sample.data);
-	}
-
-	// var verify = ESXFile.write(result, { blocksize: 1024 * 1024 * 2 });
-	// saveBuffer('data/out2.esx', verify);
-});
+// var TESTFILE = 'data/ESX-Factory-Data.esx';
+// // var TESTFILE = 'data/saved.esx';
+// fs.readFile(TESTFILE, function(err, buffer) {
+// 	if (err)
+// 		throw err;
+//
+// 	console.time('parse');
+// 	var result = ESXFile.parse(buffer);
+// 	if ('songevents' in result)
+// 		result.songevents.parse(result.songs);
+// 	if ('sampledata' in result)
+// 		result.sampledata.parse(result);
+// 	console.timeEnd('parse');
+//
+// 	function findSampleByName(name, samples) {
+// 		for (var i=0; i<samples.length; i++) {
+// 			if (!samples[i])
+// 				continue;
+// 			if (samples[i].sample.name.value == name)
+// 				return samples[i];
+// 		}
+// 		return null;
+// 	}
+//
+// 	function saveBuffer(name, buffer) {
+// 		var fd = fs.openSync(name, 'w');
+// 		fs.writeSync(fd, buffer, 0, buffer.length);
+// 		fs.closeSync(fd);
+// 	}
+//
+// 	console.time('inspect');
+// 	// console.log(result);
+// 	// console.log(require('util').inspect(result, { depth: null }));
+// 	// console.log(require('util').inspect(result.songs, { depth: null }));
+// 	// console.log(require('util').inspect(result.songevents, { depth: null }));
+// 	// console.log(require('util').inspect(result.monoSampleHeaders, { depth: null }));
+// 	// console.log(require('util').inspect(result.stereoSampleHeaders, { depth: null }));
+// 	// console.log(require('util').inspect(result.slices, { depth: null }));
+// 	// console.log(require('util').inspect(result.sampledata, { depth: null }));
+// 	console.timeEnd('inspect');
+//
+// 	var sample = findSampleByName('PercLP-1', result.sampledata.samples);
+// 	if (sample) {
+// 		console.log('Investigating sample: ', sample);
+// 		// saveBuffer('./perclp1.wav', sample.data);
+// 	}
+//
+// 	// var verify = ESXFile.write(result, { blocksize: 1024 * 1024 * 2 });
+// 	// saveBuffer('data/out2.esx', verify);
+// });
